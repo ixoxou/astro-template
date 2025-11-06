1e983da0b01d8eda1e9c7a2c4bc31853c0144a79
andrea <c.andreaboccia@icloud.com>
2025-11-06 10:50:05 +0100

    Fix Astro's config for SSR!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
-    vite: {
-        server: {
-            allowedHosts: ["levi.andrea.my", 'cleo.andrea.my']
-        },
-    }
+  vite: {
+      server: {
+          allowedHosts: ["levi.andrea.my", 'cleo.andrea.my']
+      },
+  },
+
+  adapter: cloudflare()
 });
