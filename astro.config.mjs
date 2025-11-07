a875e2b147592d79f455ea777c71903e8484d3b9
andrea <c.andreaboccia@icloud.com>
2025-11-07 13:22:37 +0100

    Add Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import sanity from "@sanity/astro";
+
 export default defineConfig({
-    vite: {
-        server: {
-            allowedHosts: ["levi.andrea.my", 'cleo.andrea.my']
-        },
-    }
+  vite: {
+      server: {
+          allowedHosts: ["levi.andrea.my", 'cleo.andrea.my']
+      },
+  },
+
+  integrations: [sanity({
+      projectId: "Milano",
+      dataset: "production",
+      useCdn: false, // for static builds
+  })]
 });
